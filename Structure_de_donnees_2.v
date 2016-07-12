5224adc05d77f070bb30359408b9491ee2dda29b
Wassim <wassim.haffaf@supelec.fr>
2016-07-12 17:13:46 +0200

    Rajout de fonctions pour add_point et correction de findIllegal

--- Structure_de_donnees_2.v
+++ Structure_de_donnees_2.v
@@ -464,6 +464,13 @@ Defined.
 
 (* findIllegal fait appel aux fonctions tr2pt et edge2index pour récupérer les 4 points *)
 
+Definition illegaltupleprop (tmap: trianglemap)
+    (p : E * point * point * point * point * T * T) :=
+   let '(x, ptext1, ptext2, ptin1, ptin2, t1, t2 (* ,
+                   preuvetriprop3  (toto (val x)) (valP t1),
+                  preuvetriprop3  (toto (val x)) (valP t2) *)) := p in
+       t1 \in tmap.
+
 Section findIllegal.
 
 Variables  (em : edgemap) (etm : edgetmap) (tmap : trianglemap) 
@@ -489,14 +496,22 @@ Let f (* : X -> option ({y : X | #|{: etm (val y)}| == 2} * point * point *
     let ptin1 := @tr2pt em tmap preuvetmap (val t1) (preuvetriprop3  (toto (val x)) (valP t1)) i1 in
     let ptin2 := @tr2pt em tmap preuvetmap (val t2) (preuvetriprop3  (toto (val x))  (valP t2)) i2 in 
       if (@inCircle ptext2 em (val t1) tmap (preuvetriprop3  (toto (val x)) (valP t1)) preuvetmap)==true 
-                      then Some (x, ptext1, ptext2, ptin1, ptin2, val t1, val t2 (* ,
-                  preuvetriprop3  (toto (val x)) (valP t1),
-                 preuvetriprop3  (toto (val x)) (valP t2) *)) else None.
+                      then  Some (exist (illegaltupleprop tmap)
+                        (val (val x), ptext1, ptext2, ptin1, ptin2, val t1, val t2 (* ,
+                   preuvetriprop3  (toto (val x)) (valP t1),
+                  preuvetriprop3  (toto (val x)) (valP t2) *))
+                   (preuvetriprop3  (toto (val x)) (valP t1)))else None.
 
 Let res := [fset f x | x in {: X} & f x != None]%fset.
 
 Definition findIllegal := match pick (pred_of_simpl (@predT {:res})) with
    Some u => val u | None => None end.
+
+
+Check (fun (tmap : trianglemap) (x : sig_choiceType (illegaltupleprop 
+tmap)) =>
+          let '(exist (x, ptext1, ptext2, ptin1, ptin2, t1, t2) _) := x in
+          t1).
 
 End findIllegal.
 
