a8ffa9482634dd7432498daacfc157078ff25dbb
Wassim <wassim.haffaf@supelec.fr>
2016-07-18 08:11:05 +0200

    <Structure de donnees à base de graphe de triangles>

---
+++ Structure_de_donnees_3.v
@@ -0,0 +1,61 @@
+Require Import Arith.
+Require Import EqNat.
+Require Import Ring.
+
+
+
+(* -------------------------------------------------------------------- *)
+From mathcomp Require Import div ssreflect eqtype ssrbool ssrnat seq finfun matrix ssrnum ssrfun fintype tuple choice path.
+From mathcomp Require Import finset zmodp matrix bigop ssralg.
+From mathcomp Require Import finmap.
+From mathcomp Require Import bigop ssralg finset fingroup zmodp poly.
+
+(* -------------------------------------------------------------------- *)
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+
+Section gensym.
+
+Open Scope fmap_scope.
+
+Definition E := nat.
+
+Definition pre_newname (e : {fset nat}) : {n : nat | n \notin e}.
+Proof.
+exists (\max_(i : e) val i + 1).
+set u := _ + 1; apply/negP => abs; move/negP: (ltnn u); apply.
+move: abs; rewrite /u addn1 => abs; rewrite ltnS.
+apply: (leq_bigmax [` abs ]%fset).
+Qed.
+
+Definition newname (e : {fset nat}) := val (pre_newname e).
+
+Lemma new_nameP e : newname e \notin e.
+Proof. rewrite /newname; apply: (@valP _ [pred n | n \notin e]). Qed.
+
+End gensym.
+
+Section Delauney.
+
+(* Notations de départ *) 
+
+Notation "zero<1" := (ltn0Sn 0).
+Notation "zero<2" := (ltn0Sn 1).
+Notation "un<2" := (ltnSn 1).
+Notation "zero<3" := (ltn0Sn 2).
+Notation "un<3" := (ltn_trans (ltnSn 1) (ltnSn 2)).
+Notation "deux<3" := (ltnSn 2).
+Notation "zero<4" := (ltn0Sn 3).
+Notation "un<4" := (ltn_trans (ltn_trans (ltnSn 1) (ltnSn 2)) (ltnSn 3)).
+Notation "deux<4" := (ltn_trans (ltnSn 2) (ltnSn 3)).
+Notation "trois<4" := (ltnSn 3).
+Notation "zero<5" := (ltn0Sn 4).
+Notation "un<5" := (ltn_trans (ltn_trans (ltn_trans (ltnSn 1) (ltnSn 2)) (ltnSn 3)) (ltnSn 4)).
+Notation "deux<5" := (ltn_trans (ltn_trans (ltnSn 2) (ltnSn 3)) (ltnSn 4)).
+Notation "trois<5" := (ltn_trans (ltnSn 3) (ltnSn 4)).
+Notation "quatre<5" := (ltnSn 4).
+
+
+
