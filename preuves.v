33bcc4e1c0b545e0e448bc62bb6acc7467745c8c
Wassim <wassim.haffaf@supelec.fr>
2016-07-21 13:06:51 +0200

    <Script contenant les preuves de l'algorithme de triangulation>

---
+++ preuves.v
@@ -0,0 +1,24 @@
+Require Import Arith.
+Require Import EqNat.
+Require Import Ring.
+
+
+
+(* -------------------------------------------------------------------- *)
+From mathcomp Require Import div ssreflect eqtype ssrbool ssrnat seq path.
+From mathcomp Require Import finset zmodp matrix bigop ssralg tuple choice.
+From mathcomp Require Import finmap seq finfun matrix ssrnum ssrfun fintype.
+From mathcomp Require Import bigop ssralg finset fingroup zmodp poly fingraph.
+
+(* -------------------------------------------------------------------- *)
+Set Implicit Arguments.
+Unset Strict Implicit.
+Unset Printing Implicit Defensive.
+
+(* -------------------------------------------------------------------- *)
+Add LoadPath "/Users/haffaf/Desktop/Stage_INRIA/DelaunayTriangulation".
+Load Structure_de_donnees_3.
+
+
+
+
