26033e0457a570a7dc6526e685f346f348e0cdff
Wassim <wassim.haffaf@supelec.fr>
2016-07-22 13:44:37 +0200

    Preuves de l'algorithme de triangulation

--- preuves.v
+++ preuves.v
@@ -50,7 +50,7 @@ Proof.
 rewrite /=.
 move=>t0 Htm_oriented.
 rewrite /add_point_triangle.
-
+Abort.
 
 
 
@@ -69,7 +69,147 @@ forall t:T, oriented t tm -> (oriented t1 tm)
         forall tt:T, oriented tt tm'
      else (oriented t1 tm) && (oriented t2 tm).
 Proof.
-
+Abort.
 
 End oriented.
+
+
+
+(* -------------------------------------------------------------------- *)
+(* Dans cette section, on va prouver l'équivalence entre être <<dans>> 
+   un triangle et s'écrire comme un barycentre des 3 sommets *)
+(* -------------------------------------------------------------------- *)
+Section barycentre.
+
+Lemma eq_bar (t:T) (tm : trianglemap) (p:point) :
+  (leftpoint ((tm t) (Ordinal(zero<3))) ((tm t) (Ordinal(un<3))) 
+                  ((tm t) (Ordinal(deux<3))) > 0)
+&& (leftpoint p ((tm t) (Ordinal(zero<3))) ((tm t) (Ordinal(un<3))) 
+                   > 0)
+&& (leftpoint p ((tm t) (Ordinal(un<3))) ((tm t) (Ordinal(deux<3))) 
+                   > 0)
+&& (leftpoint p ((tm t) (Ordinal(deux<3))) ((tm t) (Ordinal(zero<3))) 
+                  > 0)
+<-> exists k1, exists k2, exists k3,
+ (point2R1 p == k1*point2R1 ((tm t) (Ordinal(zero<3)))
+                + k2*point2R1 ((tm t) (Ordinal(un<3)))
+                + k3*point2R1 ((tm t) (Ordinal(deux<3)))) && (k1+k2+k3 ==1).
+Proof.
+Abort.
+
+
+End barycentre.
+
+
+(* -------------------------------------------------------------------- *)
+(* Dans cette section, on va s'intéresser aux surfaces qui sont conservées
+   par les opérations de flip et add_point_triangle *)
+(* -------------------------------------------------------------------- *)
+
+Section surface.
+
+
+Definition surface_sans_bord (t:T) (tm: trianglemap) (pm : pointmap) :=
+  [set nomp : P | (leftpoint (pm nomp) ((tm t) (Ordinal(zero<3)))
+                        ((tm t) (Ordinal(un<3))) >0) &&
+                  (leftpoint (pm nomp) ((tm t) (Ordinal(un<3)))
+                        ((tm t) (Ordinal(deux<3)))>0) &&
+                  (leftpoint (pm nomp) ((tm t) (Ordinal(deux<3)))
+                        ((tm t) (Ordinal(zero<3)))>0)].
+
+
+Definition bord1 (t:T) (tm: trianglemap) (pm :pointmap) :=
+[set nomp :P | (leftpoint (pm nomp) ((tm t) (Ordinal(zero<3)))
+                        ((tm t) (Ordinal(un<3))) == 0)
+                 &&  (norm_carre ((tm t) (Ordinal(zero<3))) (pm nomp)
+                              <= norm_carre ((tm t) (Ordinal(zero<3)))
+                                              ((tm t) (Ordinal(un<3))))
+                 && (prod_scal ((tm t) (Ordinal(zero<3))) (pm nomp)
+                                    ((tm t) (Ordinal(zero<3)))
+                                              ((tm t) (Ordinal(un<3))) >0)].
+
+
+
+Definition bord2 (t:T) (tm: trianglemap) (pm :pointmap) :=
+[set nomp :P | (leftpoint (pm nomp) ((tm t) (Ordinal(un<3)))
+                        ((tm t) (Ordinal(deux<3))) == 0)
+                 &&  (norm_carre ((tm t) (Ordinal(un<3))) (pm nomp)
+                              <= norm_carre ((tm t) (Ordinal(un<3)))
+                                              ((tm t) (Ordinal(deux<3))))
+                 && (prod_scal ((tm t) (Ordinal(un<3))) (pm nomp)
+                                    ((tm t) (Ordinal(un<3)))
+                                              ((tm t) (Ordinal(deux<3))) >0)].
+
+
+Definition bord3 (t:T) (tm: trianglemap) (pm :pointmap) :=
+[set nomp :P | (leftpoint (pm nomp) ((tm t) (Ordinal(deux<3)))
+                        ((tm t) (Ordinal(zero<3))) == 0)
+                 &&  (norm_carre ((tm t) (Ordinal(deux<3))) (pm nomp)
+                              <= norm_carre ((tm t) (Ordinal(deux<3)))
+                                              ((tm t) (Ordinal(zero<3))))
+                 && (prod_scal ((tm t) (Ordinal(deux<3))) (pm nomp)
+                                    ((tm t) (Ordinal(deux<3)))
+                                              ((tm t) (Ordinal(zero<3))) >0)].
+
+
+
+Definition surface (t:T) (tm: trianglemap) (pm :pointmap) :=
+  surface_sans_bord t tm pm :|: bord1 t tm pm :|: bord2 t tm pm
+        :|: bord3 t tm pm.
+
+
+
+Lemma surface_flip (tm: trianglemap)  (ptext1 : point) (ptext2 : point)
+                       (t1:T) (t2 :T) (g:graph) (pm: pointmap) (p:point) 
+                        (nomp : P) :
+pm nomp ==p
+-> (nomp \in surface t1 tm pm) 
+    || (nomp \in surface t2 tm pm)
+      -> exists t:T, if flip (tm: trianglemap) (ptext1 : point) 
+                                  (ptext2 : point) (t1:T) (t2 :T) (g:graph)
+                                     (pm: pointmap) is Some (g',tm') then
+                            nomp \in surface t tm' pm
+                   else (nomp \in surface t1 tm pm)
+                          || (nomp \in surface t2 tm pm).
+Abort.
+
+
+Lemma surface_add_point_triangle (p:point) (t:T) (tm : trianglemap)
+                                     (g:graph) (pm :pointmap) (nomp : P) :
+pm nomp ==p ->
+  (nomp \in surface t tm pm) -> if add_point_triangle (p:point) (t:T)
+                                (tm : trianglemap) (g:graph) (pm :pointmap)
+              is Some (g', tm') then exists t1:T, nomp \in surface t1 tm' pm
+                          else (nomp \in surface t tm pm).
+Abort.
+
+
+End surface.
+
+(* -------------------------------------------------------------------- *)
+(* Dans cette section, on va démontrer une propriété combinatoire *)
+(* -------------------------------------------------------------------- *)
+
+Section combi.
+
+Lemma combi_flip (tm: trianglemap) (ptext1 : point) (ptext2 : point) 
+             (t3 t4: T) (t1:T) (t2 :T) (g:graph) (pm: pointmap) (p:point) :
+ (t3 \in g t4 -> t4 \in g t3)
+-> forall (t5 t6 :T), if flip (tm: trianglemap) (ptext1 : point) 
+                                  (ptext2 : point) (t1:T) (t2 :T) (g:graph)
+                                     (pm: pointmap) is Some (g',tm') then
+                             (t5 \in g' t6 -> t6 \in g' t5)
+                   else (t3 \in g t4 -> t4 \in g t3).
+Abort.
+
+Lemma combi_add_point_triangle (p:point) (t:T) (tm : trianglemap)
+                                 (g:graph) (pm :pointmap) (t1:T) (t2 :T) :
+  (t1 \in g t2 -> t2 \in g t1)
+-> forall (t3 t4 :T), if add_point_triangle (p:point) (t:T)
+                                (tm : trianglemap) (g:graph) (pm :pointmap)
+              is Some (g', tm') then   (t3 \in g' t4 -> t4 \in g' t3)
+                          else (t1 \in g t2 -> t2 \in g t1).
+Abort.
+
+End combi.
 
