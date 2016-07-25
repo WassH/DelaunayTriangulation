c383a374c540e075b1b6f61e947bc3f87756ded9
Wassim <wassim.haffaf@supelec.fr>
2016-07-25 15:20:00 +0200

    Ecriture de la preuve point2indext1t2_correct corrigée car fausse auparavant et début de la preuve de eq_bar

--- example.v
+++ example.v
@@ -1,23 +1,53 @@
 Require Import Arith.
 Require Import EqNat.
 Require Import Ring.
+Require Import Bool.
+
+Require Import ZArith.
+Require Import Field.
 
 
+Require Import field_tactics.
 
 (* -------------------------------------------------------------------- *)
 From mathcomp Require Import div ssreflect eqtype ssrbool ssrnat seq fintype.
 From mathcomp Require Import finset zmodp matrix bigop ssralg matrix ssrnum.
 From mathcomp Require Import finmap seq ssrfun finfun matrix ssrnum ssrfun.
 From mathcomp Require Import bigop ssralg finset fingroup zmodp poly fingraph.
-From mathcomp Require Import tuple choice path.
+From mathcomp Require Import tuple choice path rat.
 (* -------------------------------------------------------------------- *)
 Set Implicit Arguments.
 Unset Strict Implicit.
 Unset Printing Implicit Defensive.
 
+Import GRing.Theory.
+Import Num.Theory.
+
+Open Scope ring_scope.
+
 Require Import Structure_de_donnees_3.
 
 Section on_map.
+
+
+(* Notations de départ *) 
+
+Notation "zero<1" := (ltn0Sn 0).
+Notation "zero<2" := (ltn0Sn 1).
+Notation "un<2" := (ltnSn 1).
+Notation "zero<3" := (ltn0Sn 2).
+Notation "un<3" := (ltn_trans (ltnSn 1) (ltnSn 2)).
+Notation "deux<3" := (ltnSn 2).
+Notation "zero<4" := (ltn0Sn 3).
+Notation "un<4" := (ltn_trans (ltn_trans (ltnSn 1) (ltnSn 2)) (ltnSn 3)).
+Notation "deux<4" := (ltn_trans (ltnSn 2) (ltnSn 3)).
+Notation "trois<4" := (ltnSn 3).
+Notation "zero<5" := (ltn0Sn 4).
+Notation "un<5" := (ltn_trans (ltn_trans (ltn_trans (ltnSn 1) (ltnSn 2))
+                                 (ltnSn 3)) (ltnSn 4)).
+Notation "deux<5" := (ltn_trans (ltn_trans (ltnSn 2) (ltnSn 3)) (ltnSn 4)).
+Notation "trois<5" := (ltn_trans (ltnSn 3) (ltnSn 4)).
+Notation "quatre<5" := (ltnSn 4).
 
 Open Scope ring_scope.
 
@@ -29,9 +59,7 @@ Definition T := T P.
 
 Check trianglemap.
 
-Definition trianglemap := trianglemap R P.
-
-Definition point := 'rV[R]_2.
+Definition trianglemap := trianglemap P.
 
 Variable default_triangle : point ^ 3.
 
@@ -41,10 +69,120 @@ Hypothesis leftpoint_default :
 
 Definition graph := T -> seq T.
 
-Definition pointmap := pointmap R P.
+Definition pointmap := pointmap P.
+
+
+
+
+Definition surface_sans_bord (t:T) (tm: trianglemap) (pm : pointmap) :=
+  [set nomp : P | (leftpoint (pm nomp) ((tm t) (Ordinal(zero<3)))
+                        ((tm t) (Ordinal(un<3))) >0) &&
+                  (leftpoint (pm nomp) ((tm t) (Ordinal(un<3)))
+                        ((tm t) (Ordinal(deux<3)))>0) &&
+                  (leftpoint (pm nomp) ((tm t) (Ordinal(deux<3)))
+                        ((tm t) (Ordinal(zero<3)))>0)].
+
+
+Definition bord1 (t:T) (tm: trianglemap) (pm :pointmap) :=
+[set nomp :P | (leftpoint (pm nomp) ((tm t) (Ordinal(zero<3)))
+                        ((tm t) (Ordinal(un<3))) == 0)
+                 &&  (norm_carre ((tm t) (Ordinal(zero<3))) (pm nomp)
+                              <= norm_carre ((tm t) (Ordinal(zero<3)))
+                                              ((tm t) (Ordinal(un<3))))
+                 && (prod_scal ((tm t) (Ordinal(zero<3))) (pm nomp)
+                                    ((tm t) (Ordinal(zero<3)))
+                                              ((tm t) (Ordinal(un<3))) >0)].
+
+
+
+Definition bord2 (t:T) (tm: trianglemap) (pm :pointmap) :=
+[set nomp :P | (leftpoint (pm nomp) ((tm t) (Ordinal(un<3)))
+                        ((tm t) (Ordinal(deux<3))) == 0)
+                 &&  (norm_carre ((tm t) (Ordinal(un<3))) (pm nomp)
+                              <= norm_carre ((tm t) (Ordinal(un<3)))
+                                              ((tm t) (Ordinal(deux<3))))
+                 && (prod_scal ((tm t) (Ordinal(un<3))) (pm nomp)
+                                    ((tm t) (Ordinal(un<3)))
+                                              ((tm t) (Ordinal(deux<3))) >0)].
+
+
+Definition bord3 (t:T) (tm: trianglemap) (pm :pointmap) :=
+[set nomp :P | (leftpoint (pm nomp) ((tm t) (Ordinal(deux<3)))
+                        ((tm t) (Ordinal(zero<3))) == 0)
+                 &&  (norm_carre ((tm t) (Ordinal(deux<3))) (pm nomp)
+                              <= norm_carre ((tm t) (Ordinal(deux<3)))
+                                              ((tm t) (Ordinal(zero<3))))
+                 && (prod_scal ((tm t) (Ordinal(deux<3))) (pm nomp)
+                                    ((tm t) (Ordinal(deux<3)))
+                                              ((tm t) (Ordinal(zero<3))) >0)].
+
+
+
+Definition surface (t:T) (tm: trianglemap) (pm :pointmap) :=
+  surface_sans_bord t tm pm :|: bord1 t tm pm :|: bord2 t tm pm
+        :|: bord3 t tm pm.
 
 Definition oriented (t : T) (tm :trianglemap) := 
   leftpoint (tm t (inZp 0)) (tm t (inZp 1)) (tm t (inZp 2)) > 0.
+
+
+(* -------------------------------------------------------------------- *)
+(* Dans cette section, on va prouver l'équivalence entre être <<dans>> 
+   un triangle et s'écrire comme un barycentre des 3 sommets *)
+(* -------------------------------------------------------------------- *)
+
+
+Lemma eq_bar (t:T) (tm : trianglemap) (p:point) 
+  (toriented  : (leftpoint ((tm t) (Ordinal(zero<3))) ((tm t) (Ordinal(un<3))) 
+                  ((tm t) (Ordinal(deux<3))) > 0)) :
+   (leftpoint p ((tm t) (Ordinal(zero<3))) ((tm t) (Ordinal(un<3))) 
+                   > 0)
+&& (leftpoint p ((tm t) (Ordinal(un<3))) ((tm t) (Ordinal(deux<3))) 
+                   > 0)
+&& (leftpoint p ((tm t) (Ordinal(deux<3))) ((tm t) (Ordinal(zero<3))) 
+                  > 0)
+<-> exists k1, exists k2, exists k3,
+ (point2R1 p = k1*point2R1 ((tm t) (Ordinal(zero<3)))
+                + k2*point2R1 ((tm t) (Ordinal(un<3)))
+                + k3*point2R1 ((tm t) (Ordinal(deux<3)))) /\
+ (point2R2 p = k1*point2R2 ((tm t) (Ordinal(zero<3)))
+                + k2*point2R2 ((tm t) (Ordinal(un<3)))
+                + k3*point2R2 ((tm t) (Ordinal(deux<3))))
+            /\ (k1+k2+k3 ==1).
+Proof.
+split; last first.
+  move=> [k1 [k2 [k3 [H1 H2]]]].
+  move:H2.
+  move=> [H2 H3].
+
+  rewrite /leftpoint.
+  to_rat_type.
+  rewrite H1 H2.
+  
+
+
+
+
+exists (leftpoint (tm t (Ordinal(zero<3))) (tm t (Ordinal(un<3)))
+                                                  (tm t (Ordinal(deux<3)))).
+exists (leftpoint (tm t (Ordinal(zero<3))) (tm t (Ordinal(un<3))) p).
+exists (leftpoint (tm t (Ordinal(un<3))) (tm t (Ordinal(deux<3))) p).
+exists (leftpoint (tm t (Ordinal(deux<3))) (tm t (Ordinal(zero<3))) p).
+rewrite /leftpoint.
+to_rat_type.
+
+
+
+Lemma oriented_triangles_after_flip (p:point) (t :T) (tm: trianglemap)  :
+   (leftpoint p ((tm t) (Ordinal(zero<3))) ((tm t) (Ordinal(un<3))) 
+                   > 0)
+&& (leftpoint p ((tm t) (Ordinal(un<3))) ((tm t) (Ordinal(deux<3))) 
+                   > 0)
+&& (leftpoint p ((tm t) (Ordinal(deux<3))) ((tm t) (Ordinal(zero<3))) 
+                  > 0) -> inCircle p t tm ==false.
+Proof.
+Abort.
+
 
 Lemma orientedunhookT (tm : trianglemap) (g : T -> seq T) t1 :
   (forall t', oriented t' tm) ->
@@ -81,12 +219,162 @@ Definition pt_in_triangle  (tm : trianglemap) p t :=
       || (p == tm t (Ordinal (ltn_trans (ltnSn 1) (ltnSn 2))))
       || (p == tm t (Ordinal (ltnSn 2))) = true.
 
-Lemma point2indext1t2_correct tm p t t1 t2 :
-    pt_in_triangle tm p t -> (t == t1) || (t == t2) ->
-    tm t (point2indext1t2 p t1 t2 tm) = p.
+
+Lemma point2indext1t2_correct tm p t1 t2 :
+    pt_in_triangle tm p t1 -> ~(pt_in_triangle tm p t2) ->
+                                     tm t1 (point2indext1t2 p t1 t2 tm) = p.
 Proof.
 (* A faire par wassim. *)
-Admitted.
+rewrite /point2indext1t2 /pt_in_triangle.
+case info: (tm t1 (Ordinal zero<3)==p).
+case info2 : (p == tm t2 (Ordinal zero<3)) => //=. move/eqP:info=>info.
+move=> Ha Hb.
+case info3 : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:info3=>info3; rewrite info3.
+case info4 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:info4=>info4; rewrite info4.
+case info5 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:info5=>info5; rewrite info5.
+case info6 : (p == tm t2 (Ordinal un<3)).
+  move:Ha.
+  rewrite info3 info5 //=.
+  Search _ (_|| false).
+  rewrite orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move:Ha.
+rewrite info3 info4 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info2: (tm t2 (Ordinal zero<3)==p) => //=. move/eqP:info=>info.
+move=> Ha Hb.
+case info3 : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:info3=>info3; rewrite info3.
+case info4 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:info4=>info4; rewrite info4.
+case info5 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:info5=>info5; rewrite info5.
+case info6 : (p == tm t2 (Ordinal zero<3)).
+  move:Ha.
+  rewrite info4 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info7 : (p == tm t2 (Ordinal un<3)).
+  move: Ha.
+  rewrite info3 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move: Ha.
+rewrite info3 info4 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info2b: (tm t2 (Ordinal un<3)==p) => //=. move/eqP:info=>info.
+move=> Ha Hb.
+case info3 : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:info3=>info3; rewrite info3.
+case info4 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:info4=>info4; rewrite info4.
+case info5 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:info5=>info5; rewrite info5.
+case info6 : (p == tm t2 (Ordinal zero<3)).
+  move:Ha.
+  rewrite info4 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info7 : (p == tm t2 (Ordinal un<3)).
+  move: Ha.
+  rewrite info3 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move: Ha.
+rewrite info3 info4 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info2bb: (tm t2 (Ordinal deux<3)==p) => //=. move/eqP:info=>info.
+move=> Ha Hb.
+case info3 : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:info3=>info3; rewrite info3.
+case info4 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:info4=>info4; rewrite info4.
+case info5 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:info5=>info5; rewrite info5.
+case info6 : (p == tm t2 (Ordinal zero<3)).
+  move:Ha.
+  rewrite info4 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info7 : (p == tm t2 (Ordinal un<3)).
+  move: Ha.
+  rewrite info3 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move: Ha.
+rewrite info3 info4 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+
+case info1b: (tm t1 (Ordinal un<3)==p) => //=. move/eqP:info=>info.
+move=> Ha Hb.
+case info3 : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:info3=>info3; rewrite info3.
+case info4 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:info4=>info4; rewrite info4.
+case info5 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:info5=>info5; rewrite info5.
+case info6 : (p == tm t2 (Ordinal zero<3)).
+  move:Ha.
+  rewrite info4 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info7 : (p == tm t2 (Ordinal un<3)).
+  move: Ha.
+  rewrite info3 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move: Ha.
+rewrite info3 info4 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+
+case info1bb: (tm t2 (Ordinal deux<3)==p) => //=. move/eqP:info=>info.
+move=> Ha Hb.
+case info3 : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:info3=>info3; rewrite info3.
+case info4 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:info4=>info4; rewrite info4.
+case info5 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:info5=>info5; rewrite info5.
+case info6 : (p == tm t2 (Ordinal zero<3)).
+  move:Ha.
+  rewrite info4 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case info7 : (p == tm t2 (Ordinal un<3)).
+  move: Ha.
+  rewrite info3 info5 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move: Ha.
+rewrite info3 info4 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move=> Ha Hb.
+case toto : (p == tm t1 (Ordinal zero<3)).
+  by move/eqP:toto=>toto; rewrite toto.
+case toto2 : (p == tm t1 (Ordinal un<3)).
+  by move/eqP:toto2=>toto2; rewrite toto2.
+case toto3 : (p == tm t1 (Ordinal deux<3)).
+  by move/eqP:toto3=>toto3; rewrite toto3.
+case toto4 : (p == tm t2 (Ordinal zero<3)).
+  move: Ha.
+  rewrite toto2 toto3 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+case toto5: (p == tm t2 (Ordinal un<3)).
+  move: Ha.
+  rewrite toto toto3 //=.
+  rewrite !orb_false_r.
+  by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+move: Ha.
+rewrite toto toto2 //=.
+by move=>hyp; move/eqP:hyp=>hyp; rewrite hyp.
+Qed.
+
+
+
 
 Lemma orientedpostflip (tm: trianglemap)  (ptext1 : point) (ptext2 : point)
                            (t1:T) (t2 :T) (g:graph) (pm: pointmap) :
@@ -154,3 +442,77 @@ have otr : leftpoint (new_tr (inZp 0))(new_tr (inZp 1))(new_tr (inZp 2)) > 0.
           || (ptext1 == tm t2 (Ordinal (ltnSn 2))) = false; last first.
       move => {new_tr info4 vtemp2istmap' temp2 tt vtemp2}.
      rewrite !point2indext1t2_correct ?eqxx ?orbT //.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(* -------------------------------------------------------------------- *)
+(* Dans cette section, on va s'intéresser aux surfaces qui sont conservées
+   par les opérations de flip et add_point_triangle *)
+(* -------------------------------------------------------------------- *)
+
+Lemma surface_flip (tm: trianglemap)  (ptext1 : point) (ptext2 : point)
+                       (t1:T) (t2 :T) (g:graph) (pm: pointmap) (p:point) 
+                        (nomp : P) :
+pm nomp ==p
+-> (nomp \in surface t1 tm pm) 
+    || (nomp \in surface t2 tm pm)
+      -> exists t:T, if flip (tm: trianglemap) (ptext1 : point) 
+                                  (ptext2 : point) (t1:T) (t2 :T) (g:graph)
+                                     (pm: pointmap) is Some (g',tm') then
+                            nomp \in surface t tm' pm
+                   else (nomp \in surface t1 tm pm)
+                          || (nomp \in surface t2 tm pm).
+Abort.
+
+
+Lemma surface_add_point_triangle (p:point) (t:T) (tm : trianglemap)
+                                     (g:graph) (pm :pointmap) (nomp : P) :
+pm nomp ==p ->
+  (nomp \in surface t tm pm) -> if add_point_triangle (p:point) (t:T)
+                                (tm : trianglemap) (g:graph) (pm :pointmap)
+              is Some (g', tm') then exists t1:T, nomp \in surface t1 tm' pm
+                          else (nomp \in surface t tm pm).
+Abort.
+
+
+
+
+(* -------------------------------------------------------------------- *)
+(* Dans cette section, on va démontrer une propriété combinatoire *)
+(* -------------------------------------------------------------------- *)
+
+
+
+Lemma combi_flip (tm: trianglemap) (ptext1 : point) (ptext2 : point) 
+             (t3 t4: T) (t1:T) (t2 :T) (g:graph) (pm: pointmap) (p:point) :
+ (t3 \in g t4 -> t4 \in g t3)
+-> forall (t5 t6 :T), if flip (tm: trianglemap) (ptext1 : point) 
+                                  (ptext2 : point) (t1:T) (t2 :T) (g:graph)
+                                     (pm: pointmap) is Some (g',tm') then
+                             (t5 \in g' t6 -> t6 \in g' t5)
+                   else (t3 \in g t4 -> t4 \in g t3).
+Abort.
+
+Lemma combi_add_point_triangle (p:point) (t:T) (tm : trianglemap)
+                                 (g:graph) (pm :pointmap) (t1:T) (t2 :T) :
+  (t1 \in g t2 -> t2 \in g t1)
+-> forall (t3 t4 :T), if add_point_triangle (p:point) (t:T)
+                                (tm : trianglemap) (g:graph) (pm :pointmap)
+              is Some (g', tm') then   (t3 \in g' t4 -> t4 \in g' t3)
+                          else (t1 \in g t2 -> t2 \in g t1).
+Abort.
+
+
+End Delaunay.
